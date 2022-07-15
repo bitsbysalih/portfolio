482888115e685d3be317e2e6132fdcee4dbccdd7
M Bukar <salih.codes@gmail.com>
2022-07-15 09:13:47 +0300

    inital commit for new branch

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="w-[100vw] h-[5vh] bg-[#b8b8b8] sticky top-0">
       <div className="flex max-w-[80%] h-[100%] justify-between items-center mx-auto">
-        <div>Logo</div>
+        <div>New logo and name</div>
         <div>Links</div>
         <div>Social links and stats</div>
       </div>
