02698a8a6e1e71a634a7cdd0e23c40984cc5fd91
M Bukar <salih.codes@gmail.com>
2022-07-15 09:11:56 +0300

    created scaffold for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-[100vw] h-[5vh] bg-[#b8b8b8] sticky top-0">
+      <div className="flex max-w-[80%] h-[100%] justify-between items-center mx-auto">
+        <div>Logo</div>
+        <div>Links</div>
+        <div>Social links and stats</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
 
+//Style import
 import "../styles/globals.css";
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+
+//Layout component imports
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />;
+      <Footer />
+    </>
+  );
+};
 
 export default MyApp;
